068826960dab92df73caa1fb38925bd0d04fd1bb
AlpBayram <57071283+alpbayram@users.noreply.github.com>
2025-03-24 17:38:11 +0300

    Library app javascript kısmını tamamladım.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 "use strict";
 const books = document.querySelector(".books");
 const addBook = document.querySelector(".add-book");
-const closeAddBook= document.querySelector(".kapat");
+const closeAddBook = document.querySelector(".kapat");
 const inputContainer = document.querySelector(".input-container");
 const titleInput = document.querySelector(".title-input");
 const authorInput = document.querySelector(".author-input");
@@ -10,87 +10,133 @@ const readSelect = document.querySelector(".read-select");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, page, read) {
 	this.id = crypto.randomUUID();
 	this.title = title;
 	this.author = author;
-	this.pages = pages;
+	this.page = page;
 	this.read = read;
 }
 
-function createAndAddBooktoArray(title, author, pages, read) {
-	const bookName = new Book(title, author, pages, read);
+function createBookAndAddToLibrary(title, author, page, read) {
+	const bookName = new Book(title, author, page, read);
 	myLibrary.push(bookName);
 }
-createAndAddBooktoArray("İrade Terbiyesi", "Jules Payot", "200", "Okunmadı");
-createAndAddBooktoArray("İrade Terbiyesi", "Jules Payot", "200", "Okunmadı");
-createAndAddBooktoArray("İrade Terbiyesi", "Jules Payot", "200", "Okunmadı");
-function addBookToScreen(title1,author1,pages1,read1){
-	let bookCard;
-	let title;
-	let author;
-	let page;
-	let read;
-	bookCard = document.createElement("div");
-		title = document.createElement("div");
-		title.textContent = title1;
-		author = document.createElement("div");
-		author.textContent =author1;
-		page = document.createElement("div");
-		page.textContent = "Sayfa Sayısı: " + pages1;
-		read = document.createElement("div");
-		read.textContent = read1;
 
-		bookCard.classList.add("book-card");
-		title.classList.add("title");
-		author.classList.add("author");
-		page.classList.add("page");
-		read.classList.add("read");
-		books.appendChild(bookCard);
-		bookCard.appendChild(title);
-		bookCard.appendChild(author);
-		bookCard.appendChild(page);
-		bookCard.appendChild(read);
+createBookAndAddToLibrary("İrade Terbiyesi", "Jules Payot", "200", "1");
+createBookAndAddToLibrary("İrade Terbiyesi", "Jules Payot", "200", "1");
+createBookAndAddToLibrary("İrade Terbiyesi", "Jules Payot", "200", "0");
+
+function renderBookCard(id, title, author, page, read) {
+	const bookCard = document.createElement("div");
+	const bookTitle = document.createElement("div");
+	const bookAuthor = document.createElement("div");
+	const bookPage = document.createElement("div");
+	const bookRead = document.createElement("div");
+	const bookDeleteButton = document.createElement("button");
+	const bookReadButton = document.createElement("button");
+
+	bookTitle.textContent = title;
+	bookAuthor.textContent = author;
+	bookPage.textContent = "Sayfa Sayısı: " + page;
+	bookRead.textContent = read == 1 ? "Okundu" : "Okunmadı";
+	bookDeleteButton.textContent = "Kitabı sil.";
+	bookReadButton.textContent = "Okunma durumunu değiştir.";
+
+	bookCard.dataset.id = id;
+
+	bookCard.classList.add("book-card");
+	bookTitle.classList.add("title");
+	bookAuthor.classList.add("author");
+	bookPage.classList.add("page");
+	bookRead.classList.add("read");
+	bookDeleteButton.classList.add("delete-button");
+	bookReadButton.classList.add("read-button");
+	books.appendChild(bookCard);
+	bookCard.appendChild(bookTitle);
+	bookCard.appendChild(bookAuthor);
+	bookCard.appendChild(bookPage);
+	bookCard.appendChild(bookRead);
+	bookCard.appendChild(bookDeleteButton);
+	bookCard.appendChild(bookReadButton);
 }
-function addBooksToScreen() {
-	
+function renderLibrary() {
 	myLibrary.forEach(function (item, index, array) {
-		addBookToScreen(item.title,item.author,item.pages,item.read)
+		renderBookCard(item.id, item.title, item.author, item.page, item.read);
 	});
 }
-addBooksToScreen();
+
+renderLibrary();
+
 addBook.addEventListener("click", function (event) {
-	
 	event.preventDefault();
 	if (inputContainer.style.display == "flex") {
-
-
-		const title =titleInput.value;
-		const author =authorInput.value;
-		const page =pageInput.value;
-		const read = readSelect.options[readSelect.selectedIndex].text;
-
-		createAndAddBooktoArray(title,author,page,read);
+		createBookAndAddToLibrary(
+			titleInput.value,
+			authorInput.value,
+			pageInput.value,
+			readSelect.options[readSelect.selectedIndex].value
+		);
 		const last = myLibrary.at(-1);
-		console.log(last)
-		addBookToScreen(last.title,last.author,last.pages,last.read)
+		console.log(last);
+		renderBookCard(last.id,last.title, last.author, last.page, last.read);
 		// titleInput.value = "";
-        // authorInput.value = "";
-        // pageInput.value = "";
+		// authorInput.value = "";
+		// pageInput.value = "";
 		// inputContainer.style.display = "none";
 	} else {
 		inputContainer.style.display = "flex";
 	}
 });
 closeAddBook.addEventListener("click", function (event) {
-	
 	event.preventDefault();
 	if (inputContainer.style.display == "flex") {
-
-
-		
-		 inputContainer.style.display = "none";
+		inputContainer.style.display = "none";
 	} else {
 		inputContainer.style.display = "flex";
+	}
+});
+
+books.addEventListener("click", function (event) {
+	if (event.target.matches(".delete-button")) {
+		event.preventDefault();
+		const hedef = event.target.closest(".book-card").attributes["data-id"].nodeValue;
+		console.log(event.target);
+
+		const willDeleteBookCards = event.target.parentElement;
+		willDeleteBookCards.remove();
+
+		const indexNumber = myLibrary.findIndex(function (item, index, array) {
+			console.log(item.id);
+			if (item.id == hedef) {
+				return true;
+			}
+		});
+
+		myLibrary.splice(indexNumber, 1);
+	} else if (event.target.matches(".read-button")) {
+		const read = event.target.parentElement.querySelector(".read");
+
+		const hedef = event.target.closest(".book-card").attributes["data-id"].nodeValue;
+
+		const findOnArray = myLibrary.find(function (item, index, array) {
+			if (item.id == hedef) {
+			
+				return true;
+			}
+		});
+		if (findOnArray.read == 1) {
+			findOnArray.read = 0;
+			read.textContent = findOnArray.read == 1 ? "Okundu" : "Okunmadı";
+			console.log(findOnArray);
+			console.log(findOnArray.read);
+		} else {
+			findOnArray.read = 1;
+			read.textContent = findOnArray.read == 1 ? "Okundu" : "Okunmadı";
+			console.log(findOnArray);
+			console.log(findOnArray.read);
+		}
+
+		
 	}
 });
